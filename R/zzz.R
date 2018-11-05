d96316167cce897baab7c053cd5e4882d8a2acd5
Dirk de Weerd <dirkdeweerd.work@gmail.com>
2018-11-05 19:32:23 +0100

    Added preloading of python libs

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
 to_set_py <- NULL
 to_graph_py <- NULL
 diamond_core <- NULL
+nx <- NULL
+np <- NULL
+scipy <- NULL
 
 
 .onLoad <- function(libname, pkgname){
+  nx <<- reticulate::import(module = "networkx", as = "nx", delay_load = T)
+  np <<- reticulate::import(module = "numpy", as = "np", delay_load = T)
+  scipy <- reticulate::import(module = "scipy", delay_load = T)
+  
+  
   path <- system.file(package = "MODifieRDev")
   diamond_module <- reticulate::import_from_path(module = "DIAMOnD_MODifieR_rt", path = path)
   
